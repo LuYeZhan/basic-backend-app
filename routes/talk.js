5fe16f7130df83286895cd3714ac3c47f8bb35fe
Lu <lu@hooli.store>
2019-08-18 15:20:15 +0200

    heroku bug

--- routes/talk.js
+++ routes/talk.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
-const Talk = require('../models/talk');
+const Talk = require('../models/Talk');
 
 const {
   isLoggedIn
